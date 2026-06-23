c616471fde0e25f0e941fd1575954e1731c6ddfe
Diego Valencia <diego.val.maya@gmail.com>
2026-06-22 22:35:03 -0600

    Basic ms structure

---
+++ src/main/java/com/ms/schneider/MsSchneiderSnowflakeApplication.java
@@ -0,0 +1,13 @@
+package com.ms.schneider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsSchneiderSnowflakeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsSchneiderSnowflakeApplication.class, args);
+	}
+
+}
